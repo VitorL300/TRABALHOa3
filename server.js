a6b96024f96f784bb67fe276dcfcb96f5caed773
Gabriel.Mastrillo <kakashigabe@gmail.comgit config --global user.name>
2024-03-23 12:49:16 -0300

    Feat(users): adiciona rota para criar usuarios

--- server.js
+++ server.js
@@ -2,6 +2,12 @@ const express = require('express');
 const app = express();
 const port = 3000
 
+app.post('/users', (req, res) => {
+    //logica para cirar usuario
+    //...
+    res.json({ message: 'Usuario Criado com Sucesso' });
+});
+
 app.listen(port, () => {
     console.log(`Servidor Rodando`);
 });
