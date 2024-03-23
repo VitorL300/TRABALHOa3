be79c5cc853854f56cddc2b099bf6a64a8918160
Gabriel.Mastrillo <kakashigabe@gmail.comgit config --global user.name>
2024-03-23 13:17:11 -0300

    refactor(users) - logica de criação de usuario extraida para uma função

--- server.js
+++ server.js
@@ -3,8 +3,10 @@ const app = express();
 const port = 3000
 
 function criarUsuario(body) {
+    //logica para criação de usuarios no banco de dados
     return {nome: 'usuario test', id: 1}
 }
+
 app.post('/users', (req, res) => {
     //logica para cirar usuario
     const usuario = criarUsuario(req.body);
