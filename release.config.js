ead8d63f056b51d8df81571a03f3168d689dbc61
Kevin Heidorn <128178416+Kevin-Heidorn@users.noreply.github.com>
2024-03-25 09:00:52 -0300

    Update release.config.js

--- release.config.js
+++ release.config.js
@@ -1,11 +1,11 @@
 module.exports = {
-    branches: ["main"],
-    plugins: [
-      "@semantic-release/commit-analyzer",
-      "@semantic-release/release-notes-generator",
-      "@semantic-release/changelog",
-      "@semantic-release/git",
-      "@semantic-release/github"
-    ]
-  };
-
+  branches: ["main"],
+  repositoryUrl: "https://github.com/Kevin-Heidorn/TRABALHO-A3",
+  plugins: [
+    "@semantic-release/commit-analyzer",
+    "@semantic-release/release-notes-generator",
+    "@semantic-release/changelog",
+    "@semantic-release/git",
+    "@semantic-release/github"
+  ]
+};
